264d98bb8335e6d664da02753262663c9fcac821
remcoveurink <remcoveurink@gmail.com>
2024-08-23 11:31:13 +0200

    swagger ui toegevoegd op basis van json specificatie

--- weekopdracht/src/main/java/bestelsysteem/model/Ingredient.java
+++ weekopdracht/src/main/java/bestelsysteem/model/Ingredient.java
@@ -1,6 +1,5 @@
 package bestelsysteem.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 
 public record Ingredient(@Id Integer id, String naam) {
